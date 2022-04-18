b1dea9271ffd8baea94a8cf8ce5dea567ce7d41b
JQ <JQ@janices-mbp.attlocal.net>
2022-04-17 19:40:24 -0700

    final

--- scraping.py
+++ scraping.py
@@ -8,7 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape_all():
     # Initiate headless driver for deployment
-    executable_path = {'executable_path': 'chromedriver.exe'}
+    executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
